67bdbd3f7b6be2b06ea3209eaae24e56dcf1228e
DanielGSilva <32908423+danielgsilva@users.noreply.github.com>
2020-05-15 15:12:07 +0100

    Added spots.h

---
+++ src/include/edges.h
@@ -0,0 +1,5 @@
+#ifndef CAL_PROJ_EDGES_H
+#define CAL_PROJ_EDGES_H
+
+#endif //CAL_PROJ_EDGES_H
+
---
+++ src/include/spots.h
@@ -0,0 +1,29 @@
+#ifndef CAL_PROJ_SPOTS_H
+#define CAL_PROJ_SPOTS_H
+
+#endif //CAL_PROJ_SPOTS_H
+
+using namespace std;
+
+enum type {PLASTIC, PAPER, GLASS, ORGANIC};
+
+class Spot {
+    type type;
+    float quantity;
+    float max_quantity;
+    Edge edge;
+    float distance;
+
+public:
+    Spot (type t, float q, float max_q, Edge e, float d) : type(t), quantity(q) max_quantity(max_q), edge(e), distance(d) {};
+
+    void set_quantity(float q);
+
+    type get_type();
+    float get_quantity();
+    float get_max_quantity();
+    Edge get_edge();
+    float get_distance();
+
+    bool fits(float q);
+};
