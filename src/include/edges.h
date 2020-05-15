b35692fe6fdfcebeb623d119a36d774897b00a7e
DanielGSilva <32908423+danielgsilva@users.noreply.github.com>
2020-05-15 15:15:21 +0100

    Added edges.h

--- src/include/edges.h
+++ src/include/edges.h
@@ -3,3 +3,19 @@
 
 #endif //CAL_PROJ_EDGES_H
 
+using namespace std;
+
+class Edge {
+    float weight;
+    int ID;
+    Vertex vi;
+    Vertex vf;
+
+public:
+    Edge (float w, int id, Vertex v1, Vertex v2) : weight(w), ID(id), vi(v1); vf(v2) {};
+
+    float get_weight();
+    float get_ID();
+    float get_vi();
+    float get_vf();
+};
