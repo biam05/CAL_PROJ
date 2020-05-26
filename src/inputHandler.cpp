b27965434621f4d43164146a981821e2da1af305
Beatriz Mendes <45185351+biam05@users.noreply.github.com>
2020-05-26 20:29:04 +0100

    Update inputHandler.cpp

--- src/inputHandler.cpp
+++ src/inputHandler.cpp
@@ -5,16 +5,14 @@ int ih_login(const vector<User> &users, const string &user, const string &pass)
     for (const auto &u : users){
         if(u.getUsername() == user){
             if(u.getPassword() == pass){
-                if(u.getType() == CLIENT)
-                    return 0;
-                else
-                    return 1;
+                if(u.getType() == CLIENT) return 0;              // if the user is a client
+                else                      return 1;              // if the user is a worker
             }
             else
-                return 2;
+                return 2;                                        // if the user typed the wrong password
         }
     }
-    return 3;
+    return 3;                                                    // if the user isn't found
 }
 
 int ih_signin(WasteApp &wasteApp, const string &user, const string &pass, const string &vertex, char role)
@@ -22,14 +20,17 @@ int ih_signin(WasteApp &wasteApp, const string &user, const string &pass, const 
     string username, password, position;
     vector<User> users = wasteApp.getUsers();
     enum userType type;
+
     for(const auto &u : users){
-        if(u.getUsername() == user)
+        if(u.getUsername() == user)         // If the user already exists
             return 1;
     }
 
     if(wasteApp.getGraphScale() == 1){
+        // 4x4 map
         ofstream ologinfile("../data/GridGraphs/4x4/userlogins.txt",fstream::app);
 
+        // Writes to the user file
         ologinfile << "\n" << user << "\n" << pass << "\n" << vertex << "\n";
         if (role == 'c'){
             ologinfile << "Client";
@@ -41,8 +42,10 @@ int ih_signin(WasteApp &wasteApp, const string &user, const string &pass, const 
         }
     }
     else {
+        // Porto map
         ofstream ologinfile("../data/Porto/userlogins.txt",fstream::app);
 
+        // Writes to the user file
         ologinfile << "\n" << user << "\n" << pass << "\n" << vertex << "\n";
         if (role == 'c'){
             ologinfile << "Client";
@@ -54,6 +57,7 @@ int ih_signin(WasteApp &wasteApp, const string &user, const string &pass, const 
         }
     }
 
+    // Adds the user to the WasteApp users
     users.emplace_back(username, password, stoi(vertex), type);
 
     wasteApp.setUsers(users);
@@ -63,17 +67,24 @@ int ih_signin(WasteApp &wasteApp, const string &user, const string &pass, const 
 
 int ih_request(vector<User>&users, const string &user, const string &type, const string &quantity){
     enum type tGlass = GLASS, tPaper = PAPER, tPlastic = PLASTIC, tOrganic = ORGANIC;
+
     for(User &u : users){
+        // Looks for the user that made the request
         if(u.getUsername() == user){
 
             vector<HouseRequest> r = u.getRequests();
+
+            // Adds a new request to the user requests
             if (type == "Organic")       r.push_back(HouseRequest(tOrganic, stof(quantity)));
             else if(type == "Glass")     r.push_back(HouseRequest(tGlass, stof(quantity)));
             else if(type == "Paper")     r.push_back(HouseRequest(tPaper, stof(quantity)));
             else if(type == "Plastic")   r.push_back(HouseRequest(tPlastic, stof(quantity)));
+
             u.setRequests(r);
 
             ofstream ologinfile("../data/requests.txt",fstream::app);
+
+            // Writes to the request file
             ologinfile << "\n" << user << "\n" << type << "\n" << quantity << "\n";
 
             return 0;
